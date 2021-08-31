8553ef86546fb4782a57bc3158c448072b0b498c
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2021-08-31 19:41:11 -0400

    basic google oauth server side client

---
+++ react_oauth/backend/services/google_oauth.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { OAuthClient } = require("google-auth-library");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const googleAuth = async (token) => {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  const payload = ticket.getPayload();
+
+  console.log(`User: ${payload.name} verified`);
+
+  const { sub, email, name } = payload;
+  const userId = sub;
+  return { userId: userId, email: email, name: name };
+};
+
+module.exports = googleAuth;
