fe1a387ab5979576edc494a8c0cb964cd1575acf
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2021-08-31 21:03:50 -0400

    now includes oauth server side verification

--- react_oauth/backend/controllers/index.js
+++ react_oauth/backend/controllers/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/user", require("./user.js"));
+router.use("/auth", require("./oauth.js"));
 
 router.get("/", (req, res) => {
   res.json(req.query);
