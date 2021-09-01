7fff65a201b448a908b9bd92a4397e3c765ba6a8
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2021-08-31 21:02:43 -0400

    Authorization to authorization-key to not conflict with Bearer token

--- react_oauth/backend/middlewares/auth.js
+++ react_oauth/backend/middlewares/auth.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const checkAuthHeader = (req, res, next) => {
-  if (req.header("Authorization") === process.env.AUTH_SECRET) {
+  if (req.header("authorization-key") === process.env.AUTH_SECRET) {
     console.log("Authorized!");
     next();
   } else {
